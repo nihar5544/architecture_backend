7ca002f257d86525b53663a128f686c6ecb87236
pv-nihar <165635623+pv-nihar@users.noreply.github.com>
2024-04-27 20:02:23 +0530

    remove versal

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,2 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-  "vercel-deploy": {
-    enabled: true,
-  },
+module.exports = () => ({
 });
--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -6,4 +6,28 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::project.project');
+module.exports = createCoreController('api::project.project',
+({ strapi }) => ({
+    /**
+     * @param {{ query: any; }} ctx
+     */
+    async find(ctx) {
+        const { query } = ctx;
+
+        const entity = await strapi.entityService.findMany(
+            "api::project.project",
+            {
+                ...query,
+                populate: {
+                   
+                    MetaTag: true,
+                },
+            }
+        );
+        // @ts-ignore
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        // @ts-ignore
+        return this.transformResponse(sanitizedEntity);
+    },
+}));
